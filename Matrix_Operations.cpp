102720919af88b0698e49c2fe0917be9ce60b586
vilcheva <viliilcheva@abv.bg>
2022-01-19 17:06:32 +0200

    Added functionality for data entry and multiplication

---
+++ Matrix_Operations.cpp
@@ -0,0 +1,32 @@
+#include "Matrix.h"
+#include <iostream>
+
+using namespace std;
+
+void Matrix::enterData()
+{
+	cin >> rows;
+	cin >> columns;
+	data = new float* [rows];
+	for (unsigned i = 0; i < rows; i++)
+	{
+		data[i] = new float [columns];
+		for (unsigned j = 0; j < columns; j++)
+		{
+			cin >> data[i][j];
+		}
+	}
+}
+
+void Matrix::MultiplyWithScalar(float scalar)
+{
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			data[i][j] *= scalar;
+		}
+	}
+	
+}
